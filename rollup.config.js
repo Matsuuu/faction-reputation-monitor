96e8c144d9b69eb84549d3d807eb63b13b2f34a6
Matsuuu <huhta.matias@gmail.com>
2022-11-14 18:09:00 +0200

    Add build

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import merge from 'deepmerge';
 import { createBasicConfig } from '@open-wc/building-rollup';
 
 const baseConfig = createBasicConfig({
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
-  outputDir: undefined
+    developmentMode: process.env.ROLLUP_WATCH === 'true',
+    outputDir: undefined
 });
 
 export default merge(baseConfig, {
-  input: './src/index.js',
+    input: './src/index.js',
     output: {
         file: "bundle.js"
     }
