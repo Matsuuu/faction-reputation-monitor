eca7b23c2ed98d7455739847f393fd34b5213a53
matias.huhta@fcg.fi <Matias.Huhta@fcg.fi>
2022-11-14 14:31:31 +0200

    Faction data with real data

--- src/FactionReputationMonitor.js
+++ src/FactionReputationMonitor.js
@@ -28,18 +28,25 @@ export class FactionReputationMonitor extends LitElement {
         }
     }
 
-    fetchCharacterReputationData() {
-        this.factionReputationData = [];
-        // TODO
+    async fetchCharacterReputationData() {
+        this.factionReputationData = await fetch(`https://pakkanen.dev/api/factions/with-reputations`)
+            .then(res => res.json());
     }
 
     render() {
         if (!this.factionReputationData) return html``;
 
+        const repsInData = this.factionReputationData.flatMap(entry =>
+            entry.reputations.flatMap(rep =>
+                rep.reputation
+            ));
+        const maxRepInCharts = Math.floor(Math.max(...repsInData) * 1.1);
+
         return this.factionReputationData.map(factionRep => html`
       <chart-js type="bar" aspect-ratio="1">
         <chart-js-title text="${factionRep.name}" size="24" padding="5"></chart-js-title>
-        <chart-js-legend align="center"></chart-js-legend>
+        <chart-js-legend align="start" position="bottom" use-point-style></chart-js-legend>
+        <chart-js-scale name="y" suggested-max="${maxRepInCharts}"></chart-js-scale>
 
         ${factionRep.reputations.map(rep => html`
             <chart-js-dataset label="${rep.faction.name}">
@@ -47,7 +54,7 @@ export class FactionReputationMonitor extends LitElement {
                 border-color="${rep.faction.hex_color}" 
                 background-color="${rep.faction.hex_color + '66'}" 
                 label="Reputation" 
-                data="15"
+                data="${rep.reputation}"
                 border-width="2"
             ></chart-js-data>
             </chart-js-dataset>
